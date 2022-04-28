8eb042f2186059aefcfcfc119d1f408fb1e19709
seongminkim2274 <seongmin.kim@haedenbridge.com>
2022-04-28 13:49:46 +0900

    Initial Commit

---
+++ Objc Playground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objc Playground
+//
+//  Created by 김성민 on 2022/04/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
